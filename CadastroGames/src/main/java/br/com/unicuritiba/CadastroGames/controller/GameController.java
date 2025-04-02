00ee1b18ac237de6d9819e44f070f210d2965e4c
Allef C. Nascimento <allef.etp@gmail.com>
2025-04-01 23:57:09 -0300

    CadastroGameCompleto
    GET, POST, DELE e PUT estão funcionando 100% (Obs.: PUT está sem um IF para checagem se o ID existe, fiz dessa forma por ser mais simples, mas pelo que pesquisei é sempre recomendado ter esse if de checagem, para que o PUT acabe não fazendo uma insernção indevida caso não encontre um item igual o que busca.)

--- CadastroGames/src/main/java/br/com/unicuritiba/CadastroGames/controller/GameController.java
+++ CadastroGames/src/main/java/br/com/unicuritiba/CadastroGames/controller/GameController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,22 +18,29 @@ import br.com.unicuritiba.CadastroGames.repositories.GameRepository;
 @RestController
 public class GameController {
 
-    @Autowired
-    GameRepository repository;
+	@Autowired
+	GameRepository repository;
 
-    @GetMapping("/games")
-    public ResponseEntity<List<Game>> getGames() {
-        return ResponseEntity.ok(repository.findAll());
-    }
+	@GetMapping("/games")
+	public ResponseEntity<List<Game>> getGames() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@PostMapping("/games")
+	public ResponseEntity<Game> adicionaGame(@RequestBody Game game) {
+		Game gameSalvo = repository.save(game);
+		return ResponseEntity.ok(gameSalvo);
+	}
+
+	@DeleteMapping("/games/{id}")
+	public void removeGame(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+
+	@PutMapping("/games/{id}")
+	public Game putGame(@PathVariable Long id, @RequestBody Game game) {
+	    game.setId(id);
+	    return repository.save(game);
+	}
     
-    @PostMapping("/games")
-    public ResponseEntity<Game> saveGame(@RequestBody Game game) { // Adicionado @RequestBody
-        Game gameSalvo = repository.save(game);
-        return ResponseEntity.ok(gameSalvo);
-    }
-
-    @DeleteMapping("/games/{id}") // Corrigido o mapeamento da URL e adicionado @PathVariable
-    public void removeGame(@PathVariable long id) {
-        repository.deleteById(id);
-    }
 }
