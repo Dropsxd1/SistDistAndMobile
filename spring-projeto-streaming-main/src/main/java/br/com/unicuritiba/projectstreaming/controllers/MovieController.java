2498040d215e50a8ecafce98d2d30937301a5e21
Allef C. Nascimento <allef.etp@gmail.com>
2025-04-22 21:27:11 -0300

    1.1 - Streaming

--- spring-projeto-streaming-main/src/main/java/br/com/unicuritiba/projectstreaming/controllers/MovieController.java
+++ spring-projeto-streaming-main/src/main/java/br/com/unicuritiba/projectstreaming/controllers/MovieController.java
@@ -1,5 +1,33 @@
 package br.com.unicuritiba.projectstreaming.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.unicuritiba.projectstreaming.models.Movie;
+import br.com.unicuritiba.projectstreaming.repositories.MovieRepository;
+
+@RestController
 public class MovieController {
+	
+	@Autowired
+	MovieRepository repository;
+
+	@GetMapping("/movies")
+	public ResponseEntity<List<Movie>> getUser(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/movies/{id}")
+	public ResponseEntity<User> getUser(@PathVariable long id){
+		return ResponseEntity.ok(repository.findById(id).get());
+	}
+	
 
 }
--- spring-projeto-streaming-main/src/main/java/br/com/unicuritiba/projectstreaming/controllers/UserController.java
+++ spring-projeto-streaming-main/src/main/java/br/com/unicuritiba/projectstreaming/controllers/UserController.java
@@ -1,12 +1,37 @@
 package br.com.unicuritiba.projectstreaming.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.unicuritiba.projectstreaming.models.User;
+import br.com.unicuritiba.projectstreaming.repositories.UserRepository;
 
 @RestController
 public class UserController {
 
-	
-	
-	
-	
+	@Autowired
+	UserRepository repository;
+
+	@GetMapping("/users")
+	public ResponseEntity<List<User>> getUser(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUser(@PathVariable long id){
+		return ResponseEntity.ok(repository.findById(id).get());
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<User> saveUser(@RequestBody User user){
+		User savedUser = repository.save(user);
+		return ResponseEntity.ok(savedUser);
+	}
 }
